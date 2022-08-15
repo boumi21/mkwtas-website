0d98097b734d52e8ed19c7c5fa1222498dad33f2
boumi21 <glorkpatate@yahoo.be>
2022-08-15 12:50:34 +0200

    Update wait.php page with v5

--- views/wait.php
+++ views/wait.php
@@ -9,10 +9,10 @@
 	<div id="wait-container">
 		<img src="assets/img/wait.png" width="320px" alt="Maintenance, please wait">
 		<h1>Coming back very soon!</h1>
-		<h3><i>mkwtas v4.1 update going on</i></h3>
+		<h3><i>mkwtas v5 update going on</i></h3>
 		<hr>
 		<p>Here is a great music while waiting ☟</p>
-		<iframe width="320" height="495" src="https://www.youtube-nocookie.com/embed/-m6qtuzVXIY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<iframe width="320" height="495" src="https://www.youtube-nocookie.com/embed/GOak9xDq_yA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 	</div>
 </body>
 
